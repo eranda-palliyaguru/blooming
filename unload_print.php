f62028e1d01154e64b7e88084bc0ad6c8f9cb906
erandas <erandasampath2000@gmil.com>
2020-12-28 12:04:54 +0530

    up

--- unload_print.php
+++ unload_print.php
@@ -33,7 +33,14 @@ $sec = "1";
     <div class="row">
       <div class="col-xs-12">
         <h2 class="page-header">
-          <i class="fa fa-globe"></i>  THIMAL ENTERPISES (pvt)LTD.
+          <i class="fa fa-globe"></i>  <?php  $result = $db->prepare("SELECT * FROM info");
+           $result->bindParam(':userid', $date);
+          				 $result->execute();
+          				 for($i=0; $row = $result->fetch(); $i++){
+          					 echo $row['name']."<br>";
+
+
+          					}?>
 
           <small class="pull-right">Date:<?php date_default_timezone_set("Asia/Colombo");
 	                                                        echo date("Y-m-d__h:ia")  ?></small>
