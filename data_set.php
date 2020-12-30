db9255ca99badcc8781d3199d713f076ff2a9581
erandas <erandasampath2000@gmil.com>
2020-12-30 11:18:08 +0530

    area code update  "Kurunegala"

--- data_set.php
+++ data_set.php
@@ -3,14 +3,14 @@ session_start();
 include('connect.php');
 date_default_timezone_set("Asia/Colombo");
 
-$result = $db->prepare("SELECT * FROM customer WHERE area='Gampaha' ");
+$result = $db->prepare("SELECT * FROM customer WHERE area='Kurunegala' ");
 $result->bindParam(':userid', $res);
 $result->execute();
 for($i=0; $row = $result->fetch(); $i++){
 $id=$row['customer_id'];
 
 
-$name=1;
+$name=2;
 
 $sql = "UPDATE customer
         SET area_id=?
