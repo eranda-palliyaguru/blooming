be74d4e966401b0efb4a6c58b544b64e723a941a
eranda-plliyaguu <erandasampath2000@gmil.com>
2021-01-10 11:26:24 +0530

    up2

--- save_customer.php
+++ save_customer.php
@@ -7,7 +7,7 @@ $a = $_POST['customer_name'];
 $b = $_POST['address'];
 $c = $_POST['contact'];
 $area_id = $_POST['area'];
-$root = $_POST['root'];
+$root_id = $_POST['root'];
 $type = $_POST['type'];
 $group = $_POST['group'];
 $credit = $_POST['credit'];
@@ -23,21 +23,19 @@ for($i=0; $row = $result->fetch(); $i++){
 $area_name=$row['name'];
 }
 
-
-
+$result = $db->prepare("SELECT * FROM root WHERE  id= :userid ");
+$result->bindParam(':userid', $root_id);
+$result->execute();
+for($i=0; $row = $result->fetch(); $i++){
+$root=$row['name'];
+}
 
 // query
 
-$sql = "INSERT INTO customer (customer_name,address,contact,area,area_id,root,type,category,acc_no,acc_name,credit_limit) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
-
+$sql = "INSERT INTO customer (customer_name,address,contact,area,area_id,root,root_id,type,category,acc_no,acc_name,credit_limit) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)";
 $q = $db->prepare($sql);
-
-$q->execute(array($a,$b,$c,$area_name,$area_id,$root,$type,$group,$acc_no,$acc_name,$credit));
-
-header("location: gps2.php");
-
-
-
+$q->execute(array($a,$b,$c,$area_name,$area_id,$root,$root_id,$type,$group,$acc_no,$acc_name,$credit));
+header("location: customer.php");
 
 
 ?>
