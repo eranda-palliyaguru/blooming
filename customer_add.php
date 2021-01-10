56898729bf416655db6dd96397b418b6c1a7ba50
eranda-plliyaguu <erandasampath2000@gmil.com>
2021-01-10 11:11:09 +0530

    customer save update

--- customer_add.php
+++ customer_add.php
@@ -4,24 +4,24 @@
 
 
 
-	
 
 
 
-    
-    
+
+
+
 
       <!-- SELECT2 EXAMPLE -->
       <div class="box box-info">
         <div class="box-header with-border">
           <h3 class="box-title">New Customer</h3>
 
-          
+
         <!-- /.box-header -->
 		<div class="form-group">
-              
+
 		<form method="post" action="save_customer.php">
-		
+
         <div class="box-body">
           <div class="row">
             <div class="col-md-6">
@@ -31,23 +31,23 @@
 				</div>
               </div>
 			   <?php date_default_timezone_set("Asia/Colombo"); ?>
-			  
-			  
+
+
 
                 <div class="col-md-6">
               <div class="form-group">
                 <label>	Addrss</label>
                 <input type="text" name="address" class="form-control pull-right"  >
-                  
+
                   </div>
 				</div>
-				
-        
-		
+
+
+
         </div>
       </div>
-	  
-	  
+
+
 	   <div class="box-body">
           <div class="row">
             <div class="col-md-6">
@@ -57,54 +57,168 @@
 				</div>
               </div>
 			   <?php date_default_timezone_set("Asia/Colombo"); ?>
-			  
-			  
+
+
 
                 <div class="col-md-6">
               <div class="form-group">
-                <label>Root</label>
-                  <select class="form-control" name="root" style="width:123px; padding:4px;" >
+                <label>Area</label>
+                  <select class="form-control" name="area" style="width:123px; padding:4px;" >
                     <?php
 					include("connect.php");
-                $result = $db->prepare("SELECT * FROM root  ");
+                $result = $db->prepare("SELECT * FROM area  ");
 		$result->bindParam(':userid', $res);
 		$result->execute();
 		for($i=0; $row = $result->fetch(); $i++){
 	?>
-		<option value="<?php echo $row['root_name'];?>"><?php echo $row['root_name']; ?>    </option>
+		<option value="<?php echo $row['id'];?>"><?php echo $row['name']; ?>    </option>
 	<?php
 				}
 			?>
-                    
+
                   </select>
                   </div>
 				</div>
-				
-        
-		
+
+				<div class="col-md-6">
+							<div class="form-group">
+
+								 <div class="input-group">
+					 <div class="input-group-addon">
+										<b >Root</b>
+									</div>
+			<select class="form-control select2" name="root"   class="form-control pull-right" >
+				<option value=""></option>
+			<?php
+								$result = $db->prepare("SELECT * FROM root ");
+		$result->bindParam(':userid', $res);
+		$result->execute();
+		for($i=0; $row = $result->fetch(); $i++){
+	?>
+		<option value="<?php echo $row['id'];?>" ><?php echo $row['name']; ?>    </option>
+	<?php	}	?>
+
+
+
+
+								</select>
+
+									</div>
+									</div>
+				</div>
+
+
+
         </div>
       </div>
-	   				  
-											  
+
+			<div class="box-body">
+							<div class="row">
+								<div class="col-md-6">
+									<div class="form-group">
+
+										 <div class="input-group">
+							 <div class="input-group-addon">
+												<b >Accounted Name</b>
+											</div>
+										<input type="text" name="acc_name"  class="form-control pull-right"  >
+
+											</div>
+											</div>
+						</div>
+						<div class="col-md-6">
+						<div class="form-group">
+
+							<div class="input-group">
+							 <div class="input-group-addon">
+												<b >Contact no (acc)</b>
+											</div>
+										<input type="text" name="acc_no"  class="form-control pull-right"  >
+											</div>
+											</div></div> </div></div>
+
+
+											<div class="box-body">
+										          <div class="row">
+											 <div class="col-md-6">
+										              <div class="form-group">
+
+										                 <div class="input-group">
+														   <div class="input-group-addon">
+										                    <b >Type</b>
+										                  </div>
+										               <select class="form-control select2" name="type"   class="form-control pull-right" >
+														   <option value="<?php echo $type; ?>"></option>
+											                <option value="1"> Channel </option>
+															<option value="2">  Commercial</option>
+										          <option value="3">Apartment</option>
+										                </select>
+
+										                  </div>
+										                  </div>
+														</div>
+
+													  <div class="col-md-6">
+										              <div class="form-group">
+
+										                 <div class="input-group">
+														   <div class="input-group-addon">
+										                    <b >Group</b>
+										                  </div>
+										      <select class="form-control select2" name="group"   class="form-control pull-right" >
+														<option value=""></option>
+												  <?php
+										                $result = $db->prepare("SELECT * FROM customer_category ");
+												$result->bindParam(':userid', $res);
+												$result->execute();
+												for($i=0; $row = $result->fetch(); $i++){
+											?>
+												<option value="<?php echo $row['id'];?>" ><?php echo $row['name']; ?>    </option>
+											<?php	}	?>
+
+
+
+
+										                </select>
+
+										                  </div>
+										                  </div>
+														</div>
+
+
+													         <div class="col-md-6">
+										              <div class="form-group">
+
+										                 <div class="input-group">
+														   <div class="input-group-addon">
+										                    <b >Credit Period</b>
+										                  </div>
+										                <input type="text" name="credit"  class="form-control pull-right"  >
+
+										                  </div>
+										                  </div>
+														</div>
+
+
+											 </div>
+										              </div>
+
       <!-- /.box -->
 <div class="form-group">
-              
 
-			  
-			  
-			  
+
 			  <input class="btn btn-info" type="submit" value="Add" >
-			  
+
 			  </form>
           <!-- /.box -->
 
         </div>
         <!-- /.col (left) -->
-       
 
-        
+
+
             <!-- /.box-body -->
-            
+
             </div>
           </div>
           <!-- /.box -->
@@ -112,6 +226,3 @@
         <!-- /.col (right) -->
       </div>
       <!-- /.row -->
-
-    
-   
--- save_customer.php
+++ save_customer.php
@@ -5,44 +5,34 @@ session_start();
 include('connect.php');
 $a = $_POST['customer_name'];
 $b = $_POST['address'];
-
 $c = $_POST['contact'];
+$area_id = $_POST['area'];
+$root = $_POST['root'];
+$type = $_POST['type'];
+$group = $_POST['group'];
+$credit = $_POST['credit'];
+$acc_no = $_POST['acc_no'];
+$acc_name = $_POST['acc_name'];
 
-$g = $_POST['root'];
 
 
-
-
-
-$result = $db->prepare("SELECT * FROM root WHERE  root_name= :userid ");
-
+$result = $db->prepare("SELECT * FROM  WHERE  id= :userid ");
 $result->bindParam(':userid', $g);
-
 $result->execute();
-
 for($i=0; $row = $result->fetch(); $i++){
-
-$d=$row['root_area'];
-
-
-
+$area_name=$row['name'];
 }
 
 
 
 
-
-$g1=2;
-
-
-
 // query
 
-$sql = "INSERT INTO customer (customer_name,address,contact,area,root,type) VALUES (:a,:b,:c,:d,:e,:e1)";
+$sql = "INSERT INTO customer (customer_name,address,contact,area,area_id,root,type,category,acc_no,acc_name,credit_limit) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
 
 $q = $db->prepare($sql);
 
-$q->execute(array(':a'=>$a,':b'=>$b,':c'=>$c,':d'=>$d,':e'=>$g,':e1'=>$g1));
+$q->execute(array($a,$b,$c,$area_name,$area_id,$root,$type,$group,$acc_no,$acc_name,$credit));
 
 header("location: gps2.php");
 
